ac20d9e6cc01c99c0d10ed3e6f44d50bf8b19f55
pivotal <hacktube9911@gmail.com>
2021-08-26 11:40:12 +0530

    Add files via upload

---
+++ recon-tools.sh
@@ -0,0 +1,29 @@
+sudo apt-get update -y 
+sudo apt-get upgrade -y
+
+sudo apt-get install python3-pip -y 
+
+
+#Installing httpx tool  
+GO111MODULE=on go get -v github.com/projectdiscovery/httpx/cmd/httpx
+
+#Installing waybackurls tool 
+go get github.com/tomnomnom/waybackurls
+
+
+#Installing subfinder tool 
+GO111MODULE=on go get -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder
+
+#Installing assetfinder tool
+go get -u github.com/tomnomnom/assetfinder
+
+#Installing anew tool
+ go get -u github.com/tomnomnom/anew
+
+ #Installing httprob tool
+ go get -u github.com/tomnomnom/httprobe
+
+#Installing meg tool
+go get -u github.com/tomnomnom/meg
+
+
